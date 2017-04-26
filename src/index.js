cc3ca9075726d4505448a355769ccbec77aed086
jindw <jindaweihd@163.com>
2017-04-26 13:54:58 +0800

    first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-export default 'Welcome to location-search'
+function search(key){
+    const re = eval("/" + key + "\=([^&]*)/;");
+    const forsearch = location.search+'&'+location.hash.substring(2);
+    return  re.exec(forsearch)?decodeURI(re.exec(forsearch)[1]):''
+}
+
+export default search;
